3e2223ae84ca3abe635611356dbb0b701137da25
Xiaoming020524 <114786819+Xiaoming020524@users.noreply.github.com>
2023-04-20 21:46:56 +0800

    Add files via upload

---
+++ test_4_20/test_4_20/test.c
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	int a = 10;
+//	int c = 212;
+//	int num = (-8 + 22) * a - 10 + c / 2;
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\121"));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[4] = { 'b','i','t' };//不完全初始化
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
+
+//int max(int n,int m)
+//{
+//	if (n > m)
+//	{
+//		return n;
+//	}
+//	else
+//	{
+//		return m;
+//	}
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入两个整数:");
+//	scanf("%d%d", &a, &b);
+//	int r = max(a, b);
+//	printf("%d\n", r);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	printf("请输入x = ");
+//	scanf("%d", &x);
+//	if (x == 0)//'=='关系运算符,'='赋值
+//	{
+//		y = 0;
+//	}
+//	else if (x > 0)
+//	{
+//		y = -1;
+//	}
+//	else
+//	{
+//		y = 1;
+//	}
+//	printf("y = %d", y);
+//	return 0;
+//}
